4c7c554ea78e9637b22d9b14bf41e71c9a211e55
iiamsgymnast <jacob.iiams@ndus.edu>
2017-05-11 22:55:37 -0500

    Update Vertex.java

--- Vertex.java
+++ Vertex.java
@@ -3,20 +3,12 @@ import java.util.ArrayList;
 public class Vertex {
  
     public ArrayList<Edge> edgeList;
-    private String city;
-    private int explore = 0;
+    public String city;
+    public boolean explored;
     public Vertex(String city){
       this.city = city;
-      this.explore = 0;
+      this.explored = false;
       this.edgeList = new ArrayList<Edge>();
-    }
-    
-    public void setCity(String town){
-      this.city = town;
-    }
-    
-    public void explored(){
-      this.explore = 1;
     }
 
     public void addEdge(Edge edge){
@@ -33,48 +25,22 @@ public class Vertex {
     public Edge getEdge(int index){
         return this.edgeList.get(index);
     }
-     
-    /**
-     *
-     * @param index The index of the edge to remove from this.edgeList
-     * @return Edge The removed Edge
-     */
+ 
     Edge removeEdge(int index){
         return this.edgeList.remove(index);
     }
-     
-    /**
-     *
-     * @param e The Edge to remove from this.edgeList
-     */
+ 
     public void removeEdge(Edge e){
         this.edgeList.remove(e);
     }
-     
-     
-    /**
-     *
-     * @return int The number of neighbors of this Vertex
-     */
-    public int getNeighborCount(){
+   
+   public int getNeighborCount(){
         return this.edgeList.size();
     }
      
-     
-    /**
-     *
-     * @return String The city of this Vertex
-     */
-    public String getCity(){
-        return this.city;
-    }
     public String toString(){
         return "Vertex " + city;
     }
- /*   public int hashCode(){
-        return this.city.hashCode();
-    }*/
-
     
     public boolean equals(Object other){
         if(!(other instanceof Vertex)){
